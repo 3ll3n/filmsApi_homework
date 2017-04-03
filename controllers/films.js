9c53480da9e9f92945f11916d016bc422372b5d7
3ll3n <ellengrafton01@gmail.com>
2017-04-03 16:54:22 +0100

    initial commit

---
+++ controllers/films.js
@@ -0,0 +1,36 @@
+//since we don't have a database we'll use our front end models at the moment
+var films = require('../client/src/models/films')();
+var Film = require('../client/src/models/film');
+var Review = require('../client/src/models/review');
+
+var express = require('express');
+var filmRouter = express.Router();
+
+// ****GET****
+filmRouter.get('/:id', function(req, res) {
+  res.json({data: films[req.params.id]});
+});
+
+filmRouter.get('/', function(req, res) {
+  res.json(films);
+});
+
+// ****PUT**** (like post in Sinatra)
+// filmRouter.put('/:id', function(req, res) {
+//   films[req.params.id] = req.body.film;
+//   res.json({data: films});
+// });
+
+// ****DELETE****
+// filmRouter.delete('/:id', function(req, res) {
+//   films.splice[req.params.id, 1];
+//   res.json({data: films});
+// });
+
+// ****POST****(add)
+// filmRouter.post('/', function(req, res) {
+//   films.push(req.body.film);
+//   res.json({data: films});
+// });
+
+module.exports = filmRouter;
